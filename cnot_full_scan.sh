91ad0b0b37acb0f3ddc268b6fdddfbbe1fa80ff0
Jinghang Ji <nanjingvincent@gmail.com>
2026-06-22 22:05:33 -0500

    Updated time limit

--- cnot_full_scan.sh
+++ cnot_full_scan.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=cnot_initial_scan_threaded
 #SBATCH --partition=caslake
 #SBATCH --account=pi-liangjiang
-#SBATCH --time=36:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
